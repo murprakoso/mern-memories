7b2b4aaf0c25e2a4d7d531ac841a53b3a659bc68
murprakoso <prakoso.clan04@gmail.com>
2020-12-18 12:52:17 +0700

    completed update post and fixed some bug in backend on update controller

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,4 @@ const url = 'http://localhost:8080/posts';
 /** method */
 export const fetchPost = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
